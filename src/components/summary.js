8f2beb3fe7684ffac43bf44dc955640fd2ee1c11
Chris Yang <cyang287@yahoo.com>
2018-04-19 14:23:20 -0400

    completed navigation bar and user summary page

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
+import {fetchData} from '../actions/question';
+
+import '../stylesheets/summary.css';
 
 export class Summary extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(fetchData(this.props.id));
+    }
+
     render() {
+
+        console.log(this.props.correct)
         return (
             <div className="summary">
-                <div className="summary-username">
+                <h2>User Information</h2>
+                <p className="summary-username">
                     Username: {this.props.username}
-                </div>
-                <div className="summary-name">Name: {this.props.name}</div>
-                <div className="summary-email">Email: {this.props.email}</div>
+                </p>
+                <p className="summary-name">
+                    Name: {this.props.name}
+                </p>
+                <p className="summary-email">
+                    Email: {this.props.email}
+                </p>
+                <p>
+                    Amount of correct Answers: {this.props.correct}
+                </p>
             </div>
         );
     }
@@ -19,9 +36,11 @@ export class Summary extends React.Component {
 const mapStateToProps = state => {
     const {currentUser} = state.auth;
     return {
+        id: state.auth.currentUser.id,
         username: state.auth.currentUser.username,
         name: currentUser.fullname,
         email: currentUser.email,
+        correct: state.question.correct,
     };
 };
 
